485a078c2b65962d92dd91cf2a672c9e2b621238
Andrej Nankov <andrejnankov@gmail.com>
2023-09-03 01:08:28 +0200

    Add new templates and set filterByTag and pagination for table in Category

--- src/symfony/src/Controller/CategoryController.php
+++ src/symfony/src/Controller/CategoryController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
-use App\PlainOldPhpObject\Category\CategoryCreateUpdatePopo;
 use App\Repository\CategoryRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
-use App\Request\Category\CategoryCreateUpdateRequest;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Request\Category\CategoryCreateUpdateRequest;
 use Symfony\Component\Serializer\SerializerInterface;
+use App\PlainOldPhpObject\Category\CategoryCreateUpdatePopo;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategoryController extends AbstractController
@@ -15,14 +17,26 @@ class CategoryController extends AbstractController
     public function __construct(
         public CategoryRepository $categoryRepository,
         public SerializerInterface $serializer,
+        public PaginatorInterface $paginator,
     )
     {
     }
 
     #[Route('/categories', name: 'app_category', methods: [Request::METHOD_GET])]
-    public function index()
+    public function index(Request $request)
     {
-        $categories = $this->categoryRepository->findAll();
+        $slug = $request->query->get('slug', "");
+
+        $categories = !empty($slug) ? $this->categoryRepository->findBySlug($slug) : $this->categoryRepository->findAll();
+
+        $categories = $this->paginator->paginate(
+            // Doctrine Query, not results
+            $categories,
+            // Define the page parameter
+            $request->query->getInt('page', 1),
+            // Items per page
+            5
+        );
         
         return $this->render('admin/category/index.html.twig', [
             'categories' => $categories
--- src/symfony/src/Repository/CategoryRepository.php
+++ src/symfony/src/Repository/CategoryRepository.php
@@ -54,17 +54,17 @@ class CategoryRepository extends ServiceEntityRepository
 //    /**
 //     * @return Category[] Returns an array of Category objects
 //     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+   public function findBySlug(string $value): array
+   {
+       return $this->createQueryBuilder('c')
+           ->andWhere('c.slug LIKE :val')
+           ->setParameter('val', '%' . $value . '%')
+           ->orderBy('c.id', 'ASC')
+           ->setMaxResults(10)
+           ->getQuery()
+           ->getResult()
+       ;
+   }
 
 //    public function findOneBySomeField($value): ?Category
 //    {
